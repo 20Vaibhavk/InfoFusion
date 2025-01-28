8ee90ec51d050fb2113bf0a553988195ba6beb54
Vaibhav <103679958+20Vaibhavk@users.noreply.github.com>
2025-01-29 00:19:29 +0530

    Update run.py

--- run.py
+++ run.py
@@ -1,13 +1,11 @@
 from flask import Flask
 from app.routes import routes
-import os  # Ensure this is imported only once
+import os
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = os.path.join(os.getcwd(), 'app/uploads')
-
-
-# Register the blueprint (only once)
 app.register_blueprint(routes)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=8080)
